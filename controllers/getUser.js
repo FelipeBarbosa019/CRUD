9a1ae4532af3d925ebe6ae7a070e5f0c41b242e7
taila-s-silva <t.snts1999@gmail.com>
2022-09-18 22:29:39 -0300

    first commit

--- controllers/getUser.js
+++
@@ -1,9 +0,0 @@
-const { users } = require("../dataBase/data");
-
-function getUser (req, res) {
-    console.table(users)
-    const activeUsers = users.filter(x => x.deleted == false);
-    res.status(200).json(activeUsers);
-}
-
-module.exports = getUser;
--- controllers/index.js
+++
@@ -1,6 +0,0 @@
-const getUser = require("./getUser")
-const postUser = require("./postUser")
-const putUser = require("./putUser")
-const deleteUser = require("./deleteUser")
-
-module.exports = { getUser, postUser, putUser, deleteUser }
